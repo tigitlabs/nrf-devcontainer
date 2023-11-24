9815c87c8cac8e6bb6dae6c64f4e06cd52547533
Timur <timur@tigitlabs.com>
2023-11-24 15:51:22 +0000

    feat: hello world build test

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ## Hello World
 
-[ncs-example-application](https://github.com/nrfconnect/ncs-example-application)
+[zephyr/samples/hello_world][hello_world_doc]
 
 '''bash
 ./build_test.sh
 '''
+
+[hello_world_doc]: https://developer.nordicsemi.com/nRF_Connect_SDK/doc/2.5.0/zephyr/samples/hello_world/README.html
--- build_test.sh
+++ build_test.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # build_test.sh
 
-west init -m https://github.com/nrfconnect/ncs-example-application --mr main my-workspace
-cd my-workspace
-west update
-west build -b custom_board app -- -DOVERLAY_CONFIGg.conf
+if [ -z "$ZEPHYR_BASE" ]; then
+  echo "⚠️ ZEPHYR_BASE is not set"
+  echo "Run activate the environment"
+  exit 1
+fi
+
+cp -r ~/ncs/v2.5.0/zephyr/samples/hello_world/ .
+west build hello_world --board nrf52840dk_nrf52840 --build-dir hello_world/build --pristine
+
+echo "🧪 Check if the build is successful"
+# Check if the hex file exists
+if [ -f "hello_world/build/zephyr/zephyr.hex" ]; then
+  echo "✅ Hex file exists"
+else
+  echo "❌ Hex file does not exist"
+  exit 1
+fi
